eb020f499c7c617892c6246d83931aa55830ebb5
Jonathan Holst <97708227+JonathanNoH@users.noreply.github.com>
2024-04-28 12:22:04 -0600

    implement character on screen

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,32 @@ async fn main() {
         &[],
         ).unwrap();
     println!("created tiled_map");
+    let npc = load_texture("assets/disciple-45x51.png").await.unwrap();
+
+    let width = 480.;
+    let height = 320.;
     loop {
         clear_background(BLACK);
         tiled_map.draw_tiles(
             "Background",
-            Rect::new(0.0, 0.0, screen_width(), screen_height()),
+            Rect::new(0.0, 0.0, width, height),
             None,
-            );
+        );
         tiled_map.draw_tiles(
             "Tile Layer 1",
-            Rect::new(0.0, 0.0, screen_width(), screen_height()),
+            Rect::new(0.0, 0.0, width, height),
             None,
-            );
+        );
+        draw_texture_ex(
+            &npc,
+            0.,
+            0.,
+            WHITE,
+            DrawTextureParams {
+                source: Some(Rect::new(0.0, 0.0, 45., 51.)),
+                ..Default::default()
+            },
+        );
         next_frame().await
     }
 }
