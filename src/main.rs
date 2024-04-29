3747cb7bf7aa1f134a04f11d2f9e5fb418d3170d
JonathanNoH <97708227+JonathanNoH@users.noreply.github.com>
2024-04-29 15:35:46 -0600

    player now own struct

--- src/main.rs
+++ src/main.rs
@@ -2,58 +2,84 @@ use macroquad::prelude::*;
 
 use macroquad_tiled as tiled;
 
-#[macroquad::main("BasicShapes")]
-async fn main() {
-    let tileset: Texture2D = load_texture("assets/basictiles.png").await.unwrap();
-    println!("loaded tileset");
+struct Player {
+    speed: Vec2,
+    texture: Texture2D,
+    pos: Vec2,
+}
 
-    let tiled_map_json = load_string("assets/testtilemap.json").await.unwrap();
-    println!("loaded json");
-    let tiled_map = tiled::load_map(
-        &tiled_map_json,
-        &[("basictiles.png", tileset)],
-        &[],
-        ).unwrap();
-    println!("created tiled_map");
-    let npc = load_texture("assets/disciple-45x51.png").await.unwrap();
-    let mut npc_pos = vec2(240., 160.);
-
-    let width = 480.;
-    let height = 320.;
-    loop {
-        clear_background(BLACK);
-        tiled_map.draw_tiles(
-            "Background",
-            Rect::new(0.0, 0.0, width, height),
-            None,
-        );
-        tiled_map.draw_tiles(
-            "Tile Layer 1",
-            Rect::new(0.0, 0.0, width, height),
-            None,
-        );
-        if is_key_down(KeyCode::W) {
-           npc_pos.y -= 1.0;
+impl Player {
+    pub const MOVE_SPEED: f32 = 5.0;
+    fn new(pos: Vec2, texture: Texture2D) -> Self {
+        Self {
+            texture,
+            speed: vec2(0., 0.),
+            pos,
         }
-        if is_key_down(KeyCode::A) {
-            npc_pos.x -= 1.0;
-        }
-        if is_key_down(KeyCode::S) {
-            npc_pos.y += 1.0;
-        }
-        if is_key_down(KeyCode::D) {
-            npc_pos.x += 1.0;
-        }
-        draw_texture_ex(
-            &npc,
-            npc_pos.x,
-            npc_pos.y,
+    }
+    
+    fn draw(&self) {
+    draw_texture_ex(
+            &self.texture,
+            self.pos.x,
+            self.pos.y,
             WHITE,
             DrawTextureParams {
                 source: Some(Rect::new(0.0, 0.0, 45., 51.)),
                 ..Default::default()
             },
         );
+    }
+    fn update(&mut self) {
+        if is_key_down(KeyCode::W) {
+            self.pos.y -= Self::MOVE_SPEED;
+        }
+        if is_key_down(KeyCode::A) {
+            self.pos.x -= Self::MOVE_SPEED;
+        }
+        if is_key_down(KeyCode::S) {
+            self.pos.y += Self::MOVE_SPEED;
+        }
+        if is_key_down(KeyCode::D) {
+            self.pos.x += Self::MOVE_SPEED;
+        }
+    }
+}
+
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    let tileset: Texture2D = load_texture("assets/basictiles.png").await.unwrap();
+    tileset.set_filter(FilterMode::Nearest);
+    let tiled_map_json = load_string("assets/testtilemap.json").await.unwrap();
+    let tiled_map = tiled::load_map(
+        &tiled_map_json,
+        &[("basictiles.png", tileset)],
+        &[],
+        ).unwrap();
+
+    let player = load_texture("assets/disciple-45x51.png").await.unwrap();
+
+    let player_pos = vec2(240., 160.);
+    let mut player = Player::new(player_pos, player);
+
+
+    let width = tiled_map.raw_tiled_map.tilewidth * tiled_map.raw_tiled_map.width;
+    let height = tiled_map.raw_tiled_map.tileheight * tiled_map.raw_tiled_map.height;
+
+    loop {
+        clear_background(BLACK);
+        tiled_map.draw_tiles(
+            "Background",
+            Rect::new(0.0, 0.0, width as _, height as _),
+            None,
+        );
+        tiled_map.draw_tiles(
+            "Tile Layer 1",
+            Rect::new(0.0, 0.0, width as _, height as _),
+            None,
+        );
+        player.update();
+        player.draw(); 
         next_frame().await
     }
 }
